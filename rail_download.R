c6b4745306fee19af803ed303756cadb752058e9
jess-es02 <jessicaestatt@gmail.com>
2025-06-02 19:05:23 +0100

    (hopefully) finish overground platform fixing

--- rail_download.R
+++ rail_download.R
@@ -229,19 +229,7 @@ trip_directions <- read_csv("data/trip_directions_unique_stations.csv")%>%
   clean_names()%>%
   select(-first_stop_name, -last_stop_name)
 
-#Retrospectively change some directions because it looks like TfL hasn't updated their timetable info
-#XXX
-
-#Join to trip information
-trip_id_lookup <- gtfs_nr$stop_times %>%
-  group_by(trip_id) %>%
-  arrange(stop_sequence) %>%
-  summarise(first_stop = first(stop_id),
-            last_stop = last(stop_id))
-trip_id_lookup <- trip_id_lookup %>%
-  left_join(trip_directions, by = c("first_stop", "last_stop"))
-
-#Before we join to stop_times, we need to check whether some stations have multiple overground platforms in the same direction
+#Before we join directions to stop_times, we need to check whether some stations have multiple overground platforms in the same direction
 direction_test <- gtfs_nr_stops_join %>%
   group_by(tfl_id, cardinal_direction) %>%
   summarise(platform_count = n_distinct(platform_id), .groups = "drop") %>%
@@ -298,41 +286,222 @@ gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
 #Note there are still some stops with multiple Overground routes in the same cardinal direction
 #Let's join anyway and manually fix those later
 
+#Some of the TfL platform information is either incorrect or out of date - we need to manually fix it
+#Imperial Wharf
+gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
+  mutate(
+    cardinal_direction = case_when(
+      nr_id == 'CSEAH' & platform_number == '2' ~ 'Eastbound',
+      nr_id == 'CSEAH' & platform_number == '1' ~ 'Westbound',
+      TRUE ~ cardinal_direction))
+#West Brompton
+gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
+  mutate(
+    cardinal_direction = case_when(
+      nr_id == 'WBRMPTN' & platform_number == '4' ~ 'Eastbound',
+      nr_id == 'WBRMPTN' & platform_number == '3' ~ 'Westbound',
+      TRUE ~ cardinal_direction))
+#Kensington (Olympia)
+gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
+  mutate(
+    cardinal_direction = case_when(
+      nr_id == 'KENOLYM' & platform_number == '2' ~ 'Eastbound',
+      nr_id == 'KENOLYM' & platform_number == '3' ~ 'Westbound',
+      TRUE ~ cardinal_direction))
+#Shepherd's Bush
+gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
+  mutate(
+    cardinal_direction = case_when(
+      nr_id == 'SHPDSB' & platform_number == '2' ~ 'Eastbound',
+      nr_id == 'SHPDSB' & platform_number == '1' ~ 'Westbound',
+      TRUE ~ cardinal_direction))
+#Upminster
+gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
+  mutate(
+    cardinal_direction = case_when(
+      nr_id == 'UPMNSP6' & platform_number == '6' ~ 'Westbound',
+      TRUE ~ cardinal_direction))
+#Peckham Rye
+gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
+  mutate(
+    cardinal_direction = case_when(
+      nr_id == 'PKHMRYC' & platform_number == '2' ~ 'Northbound',
+      nr_id == 'PKHMRYC' & platform_number == '1' ~ 'Southbound',
+      TRUE ~ cardinal_direction))
+#Denmark Hill
+gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
+  mutate(
+    cardinal_direction = case_when(
+      nr_id == 'DENMRKH' & platform_number == '2' ~ 'Northbound',
+      nr_id == 'DENMRKH' & platform_number == '1' ~ 'Southbound',
+      TRUE ~ cardinal_direction))
+#Clapham High Street
+gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
+  mutate(
+    cardinal_direction = case_when(
+      nr_id == 'CLPHHS' & platform_number == '2' ~ 'Northbound',
+      nr_id == 'CLPHHS' & platform_number == '1' ~ 'Southbound',
+      TRUE ~ cardinal_direction))
+#Wandsworth Road
+gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
+  mutate(
+    cardinal_direction = case_when(
+      nr_id == 'WNDSWRD' & platform_number == '2' ~ 'Northbound',
+      nr_id == 'WNDSWRD' & platform_number == '1' ~ 'Southbound',
+      TRUE ~ cardinal_direction))
+
 #Append direction onto stop times
-#Adding logic for stops at the start/end of line - reverse cardinal direction
+#First, join directions to trip information
+trip_id_lookup <- gtfs_nr$stop_times %>%
+  group_by(trip_id) %>%
+  arrange(stop_sequence) %>%
+  summarise(first_stop = first(stop_id),
+            last_stop = last(stop_id))
+trip_id_lookup <- trip_id_lookup %>%
+  left_join(trip_directions, by = c("first_stop", "last_stop"))
+
+#Artificially remove duplicate platforms which we'll need to manually fix later
+#Rerun duplicate test
+direction_test <- gtfs_nr_stops_join %>%
+  group_by(tfl_id, cardinal_direction) %>%
+  summarise(platform_count = n_distinct(platform_id), .groups = "drop") %>%
+  filter(platform_count > 1)
+#Remove the first instance from the platform list
+temp_gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
+  left_join(direction_test, by = c("tfl_id", "cardinal_direction"))%>%
+  group_by(tfl_id, cardinal_direction)%>%
+  filter(is.na(platform_count) | row_number() != 1) %>%
+  ungroup()%>%
+  select(-platform_count)
+
+#Adding dictionary logic for stops at the start/end of line - reverse cardinal direction
 directions <- c("Northbound"="Southbound", "Southbound"="Northbound", "Eastbound"="Westbound", "Westbound"="Eastbound")
 
 gtfs_nr_stop_times <- gtfs_nr$stop_times
 gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
   left_join(trip_id_lookup, by=c("trip_id"))%>%
-  mutate(cardinal_direction = if_else(drop_off_type == 1 | pickup_type == 1, #invert platform at the start/end of line
+  mutate(cardinal_direction = if_else(pickup_type == 1, #invert platform at the end of line - so it "stops" in the same place it starts
                               directions[cardinal_direction],
                               cardinal_direction))%>%
-  left_join(gtfs_nr_stops_join, by = c("stop_id" = "nr_id", "cardinal_direction"))
+  left_join(temp_gtfs_nr_stops_join, by = c("stop_id" = "nr_id", "cardinal_direction"))
+
 #Update logic for Battersea Park, which was excluded from platform dataset
 gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
   mutate(platform_id = if_else(stop_id == 'BATRSPK', 'BATRSPK', platform_id))
 
-#Things to sort:
-#1) Many join - artificially remove duplicate direction platforms before reintroducing?
-  # Go to line 233
-#2) Some stations in wrong directions?
-#3) Manually sort duplicate direction platforms?
+#Manually updating further stations
+#Liverpool Street: all one platform, randomly-chosen because they differ
+gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
+  mutate(tfl_id = if_else(stop_id == 'LIVST', 'HUBLST', tfl_id),
+         stop_name = if_else(stop_id == 'LIVST', 'London Liverpool Street Rail Station', stop_name),
+         stop_lon = if_else(stop_id == 'LIVST', -0.08143, stop_lon),
+         stop_lat = if_else(stop_id == 'LIVST', 51.51799, stop_lat),
+         platform_id = if_else(stop_id == 'LIVST', 'HUBLST-Plat01-EB-london-overground', platform_id),
+         platform_number = if_else(stop_id == 'LIVST', '1', platform_number))
 
-view(gtfs_nr_stop_times %>% filter(stop_id == 'BATRSPK'))
-#Stops with multiple Overground routes in the same cardinal direction:
-# - Gospel Oak 910GGOSPLOK
-# - Hackney Downs is not on multiple lines, but will require a bit of careful consideration as it's a key interchange within the Weaver line 910GHAKNYNM
-# - Clapham Junction HUBCLJ
-# - Highbury and Islington HUBHHY
-# - Stratford - same line, but some to Richmond and some to Clapham Junction HUBSRA
-# - Willesden Junction HUBWIJ
+#Bethnal Green - what is actually Northbound has an Eastbound platform (2)
+gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
+  mutate(tfl_id = if_else(stop_id == 'BTHNLGR' & stop_sequence == 2, '910GBTHNLGR', tfl_id),
+         stop_name = if_else(stop_id == 'BTHNLGR' & stop_sequence == 2, 'Bethnal Green Rail Station', stop_name),
+         stop_lon = if_else(stop_id == 'BTHNLGR' & stop_sequence == 2, -0.05957, stop_lon),
+         stop_lat = if_else(stop_id == 'BTHNLGR' & stop_sequence == 2, 51.52392, stop_lat),
+         platform_id = if_else(stop_id == 'BTHNLGR' & stop_sequence == 2, '910GBTHNLGR-Plat02-EB-london-overground', platform_id),
+         platform_number = if_else(stop_id == 'BTHNLGR' & stop_sequence == 2, '2', platform_number))
+#And what is Southbound has a Westbound platform (1)
+gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
+  mutate(tfl_id = if_else(stop_id == 'BTHNLGR' & stop_sequence > 2, '910GBTHNLGR', tfl_id),
+         stop_name = if_else(stop_id == 'BTHNLGR' & stop_sequence > 2, 'Bethnal Green Rail Station', stop_name),
+         stop_lon = if_else(stop_id == 'BTHNLGR' & stop_sequence > 2, -0.05957, stop_lon),
+         stop_lat = if_else(stop_id == 'BTHNLGR' & stop_sequence > 2, 51.52392, stop_lat),
+         platform_id = if_else(stop_id == 'BTHNLGR' & stop_sequence > 2, '910GBTHNLGR-Plat01-WB-london-overground', platform_id),
+         platform_number = if_else(stop_id == 'BTHNLGR' & stop_sequence > 2, '1', platform_number))
 
+#Inspect remaining nulls
+check_nulls <- gtfs_nr_stop_times %>%
+  filter(is.na(platform_id))
 
-#Clean workspace when done
+#Some trips are clearly invalid - they jump in space between Mildmay and Lioness
+#We will probably have to remove these entirely - a limitation of the data
+problematic_trips <- check_nulls %>%
+  filter(stop_id %in% c("KLBRNHR", "SHMPSTD", "KENSLG", "QPRK"))
+problematic_trip_ids <- unique(problematic_trips$trip_id)
+gtfs_nr$trips <- gtfs_nr$trips %>%
+  filter(!trip_id %in% problematic_trip_ids)
+gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
+  filter(!trip_id %in% problematic_trip_ids)
+#And some invalid trips jumping from the Suffragette to Mildmay
+more_problematic_ids <- c(336006, 370144)
+gtfs_nr$trips <- gtfs_nr$trips %>%
+  filter(!trip_id %in% more_problematic_ids)
+gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
+  filter(!trip_id %in% more_problematic_ids)
+
+#Manually sort remaining nulls
+gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
+  mutate(tfl_id = if_else((first_stop == 'CLPHMJC' & last_stop == 'DALS') | (first_stop == 'DALS' & last_stop == 'CLPHMJC'), 'HUBCLJ', tfl_id),
+         stop_name = if_else((first_stop == 'CLPHMJC' & last_stop == 'DALS') | (first_stop == 'DALS' & last_stop == 'CLPHMJC'), 'Clapham Junction Rail Station', stop_name),
+         stop_lon = if_else((first_stop == 'CLPHMJC' & last_stop == 'DALS') | (first_stop == 'DALS' & last_stop == 'CLPHMJC'), -0.17017, stop_lon),
+         stop_lat = if_else((first_stop == 'CLPHMJC' & last_stop == 'DALS') | (first_stop == 'DALS' & last_stop == 'CLPHMJC'), 51.46415, stop_lat),
+         platform_id = if_else((first_stop == 'CLPHMJC' & last_stop == 'DALS') | (first_stop == 'DALS' & last_stop == 'CLPHMJC'), 'HUBCLJ-Plat02-EB-london-overground', platform_id),
+         platform_number = if_else((first_stop == 'CLPHMJC' & last_stop == 'DALS') | (first_stop == 'DALS' & last_stop == 'CLPHMJC'), '2', platform_number))
+gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
+  mutate(tfl_id = if_else((first_stop == 'CLPHMJC' & last_stop == 'HIGHBYA') | (first_stop == 'HIGHBYA' & last_stop == 'CLPHMJC'), 'HUBCLJ', tfl_id),
+         stop_name = if_else((first_stop == 'CLPHMJC' & last_stop == 'HIGHBYA') | (first_stop == 'HIGHBYA' & last_stop == 'CLPHMJC'), 'Clapham Junction Rail Station', stop_name),
+         stop_lon = if_else((first_stop == 'CLPHMJC' & last_stop == 'HIGHBYA') | (first_stop == 'HIGHBYA' & last_stop == 'CLPHMJC'), -0.17017, stop_lon),
+         stop_lat = if_else((first_stop == 'CLPHMJC' & last_stop == 'HIGHBYA') | (first_stop == 'HIGHBYA' & last_stop == 'CLPHMJC'), 51.46415, stop_lat),
+         platform_id = if_else((first_stop == 'CLPHMJC' & last_stop == 'HIGHBYA') | (first_stop == 'HIGHBYA' & last_stop == 'CLPHMJC'), 'HUBCLJ-Plat02-EB-london-overground', platform_id),
+         platform_number = if_else((first_stop == 'CLPHMJC' & last_stop == 'HIGHBYA') | (first_stop == 'HIGHBYA' & last_stop == 'CLPHMJC'), '2', platform_number))
+
+#Now we need to manually fix the stations with platforms in the same direction
+
+#Gospel Oak Eastbound: 3 should be Suffragette, 2 should be Mildmay
+platform_codes <- c("STFD", "CMDNRD")
+gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
+  mutate(platform_id = if_else((platform_id == "910GGOSPLOK-Plat03-EB-london-overground" & (first_stop %in% platform_codes |last_stop %in% platform_codes)), "910GGOSPLOK-Plat02-EB-london-overground", platform_id),
+         platform_number = if_else(platform_id == '910GGOSPLOK-Plat02-EB-london-overground', "2", platform_number))
+
+#Hackney Downs Northbound: platform 2 to Chingford, 4 to Enfield/Cheshunt
+gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
+  mutate(platform_id = if_else(platform_id == "910GHAKNYNM-Plat04-NB-london-overground" & last_stop == "CHINGFD", "910GHAKNYNM-Plat02-NB-london-overground", platform_id),
+         platform_number = if_else(platform_id == '910GHAKNYNM-Plat02-NB-london-overground', "2", platform_number))
+
+#Hackney Downs Southbound
+#It looks like these all go to the same destination - platform 3 is more frequently used than 1, so let's keep it this way
+gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
+  filter(!platform_id == '910GHAKNYNM-Plat01-SB-london-overground')
+
+#Clapham Junction Eastbound: platform 1 is Mildmay (Stratford), 2 is Windrush (Dalston)
+platform_codes <- c("STFD", "WLSDJHL", "GOSPLOK", "CMDNRD", "SHPDSB")
+gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
+  mutate(platform_id = if_else((platform_id == "HUBCLJ-Plat02-EB-london-overground" & (first_stop %in% platform_codes |last_stop %in% platform_codes)), "HUBCLJ-Plat01-EB-london-overground", platform_id),
+         platform_number = if_else(platform_id == 'HUBCLJ-Plat01-EB-london-overground', "1", platform_number))
+
+#Stratford Westbound: 1 to Richmond/Camden, 2 to Clapham
+platform_codes <- c("RICHNLL", "CMDNRD", "ACTNCTL")
+gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
+  mutate(platform_id = if_else((platform_id == "HUBSRA-Plat02-WB-london-overground" & (first_stop %in% platform_codes |last_stop %in% platform_codes)), "HUBCLJ-Plat01-EB-london-overground", platform_id),
+         platform_number = if_else(platform_id == 'HUBCLJ-Plat01-EB-london-overground', "1", platform_number))
+
+#Willesden Junction Eastbound
+#Looks like platform 2 is rarely used - let's remove it
+gtfs_nr_stops_join <- gtfs_nr_stops_join %>%
+  filter(!platform_id == 'HUBWIJ-Plat02-EB-london-overground')
+
+#Highbury and Islington Southbound: 1 to West Croydon, 2 to Crystal Palace
+platform_codes <- c("WCROYDN", "BATRSPK")
+gtfs_nr_stop_times <- gtfs_nr_stop_times %>%
+  mutate(platform_id = if_else((platform_id == "HUBHHY-Plat02-SB-london-overground" & (first_stop %in% platform_codes |last_stop %in% platform_codes)), "HUBHHY-Plat01-SB-london-overground", platform_id),
+         platform_number = if_else(platform_id == 'HUBHHY-Plat01-SB-london-overground', "1", platform_number))
+
+#Limitations: for routes terminating before one of these, it is unclear which platform it departs from (as I'm comparing to live departures)
+#Another limitation was that because some of the TfL directions were incorrect or inconsistent, we don't know if they all joined correctly (we only found errors if there were nulls - what about multiple lines at the same station?)
+
+rm(check_nulls, direction_test, gtfs_nr_stops, problematic_trips, temp_gtfs_nr_stops_join, trip_directions, trip_id_lookup, directions, more_problematic_ids, platform_codes, problematic_trip_ids)
 
 #Main tasks:
 # - Create new gtfs_nr$stops, with a new stop which can link to TfL platform info
+  # - Need to update gtfs_nr$stop_times and gtfs_nr$stops in GTFS object
+  # - Ensure to validate network afterwards
 # - Turn London GTFS data into TfL platforms (should be easier!)
 # - Merge GTFS files, create new r5r_core
 
